1bdb7474439d1cf4b43da538c6a61d49a2ccdcba
vuong phan <ducvuong.dev@gmail.com>
2020-09-17 23:04:48 +0700

    fix model level

--- app/models/level_model.go
+++ app/models/level_model.go
@@ -2,8 +2,8 @@ package models
 
 type LevelModel struct {
 	Time			int64		`json:"time"`
-	HighScore	int64		`json:"high_score"`
+	HighScore	int64		`json:"high_score" bson:"high_score"`
 	Combo			int			`json:"combo"`
-	BestCombo	int			`json:"best_combo"`
-	IDLevel		int			`json:"id_level"`
+	BestCombo	int			`json:"best_combo" bson:"best_combo"`
+	IDLevel		int			`json:"id_level" bson:"id_level"`
 }
